20c2b1eb7ea6eccbd406395ce71a24e1d755e78e
Sun <wanglt-123@qq.com>
2025-09-03 17:02:35 +0800

    refactor(config): 更新 API 前缀环境变量键名
    将环境变量键名从 APP_PREFIX 改为 API_PREFIX，以更好地反映其用途。

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -7,7 +7,7 @@ export const appRegToken = 'app';
 export const AppConfig = registerAs(appRegToken, () => ({
   name: env('APP_NAME'),
   port: envNumber('APP_PORT', 3000),
-  prefix: env('APP_PREFIX', '/api'),
+  prefix: env('API_PREFIX', '/api'),
 }));
 
 export type IAppConfig = ConfigType<typeof AppConfig>;
