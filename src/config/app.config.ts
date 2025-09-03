61fd307223763a54ba0fb5fc7bff8b43b862fba8
Sun <wanglt-123@qq.com>
2025-09-03 17:20:45 +0800

    feat(config): 添加应用配置项
    - 在 AppConfig 中添加了系统名称、端口号和接口前缀等配置项
    - 更新了 ListService 中的 findOne、update 和 remove 方法，增加了数据存在性检查和异常处理

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -5,8 +5,11 @@ import { env, envNumber } from '@/utils/globalenv.utils';
 export const appRegToken = 'app';
 
 export const AppConfig = registerAs(appRegToken, () => ({
+  // 系统名称
   name: env('APP_NAME'),
+  // 端口号
   port: envNumber('APP_PORT', 3000),
+  // 接口前缀
   prefix: env('API_PREFIX', '/api'),
 }));
 
--- src/modules/list/list.service.ts
+++ src/modules/list/list.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+
 import { PrismaService } from '@/database/database.service';
 import { CreateListDto } from './dto/create-list.dto';
 import { UpdateListDto } from './dto/update-list.dto';
@@ -18,14 +19,39 @@ export class ListService {
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} list`;
+    const data = await this.prisma.list.findUnique({ where: { id } });
+
+    if (!data) throw new HttpException('没有找到该数据', HttpStatus.NOT_FOUND);
+
+    return data;
   }
 
   async update(id: number, updateListDto: UpdateListDto) {
-    return `This action updates a #${id} list`;
+    const exists = await this.prisma.list.findUnique({ where: { id } });
+    if (!exists)
+      throw new HttpException('没有找到该数据', HttpStatus.NOT_FOUND);
+
+    const data = await this.prisma.list.update({
+      where: { id },
+      data: updateListDto,
+    });
+
+    if (!data)
+      throw new HttpException('修改失败', HttpStatus.INTERNAL_SERVER_ERROR);
+    return data;
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} list`;
+    const exists = await this.prisma.list.findUnique({ where: { id } });
+    if (!exists)
+      throw new HttpException('没有找到该数据', HttpStatus.NOT_FOUND);
+
+    const data = await this.prisma.list.delete({ where: { id } });
+    if (!data)
+      throw new HttpException('删除失败', HttpStatus.INTERNAL_SERVER_ERROR);
+
+    return {
+      msg: '删除成功',
+    };
   }
 }
