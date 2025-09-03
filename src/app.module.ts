132bf7d1fe3c5bd7dbc2d75ee49cf7eb2e07b636
Sun <wanglt-123@qq.com>
2025-09-03 15:11:26 +0800

    feat(config): 添加环境变量支持并优化项目配置
    - 引入 @nestjs/config 模块，支持环境变量管理
    - 更新 .gitignore 文件，移除对环境变量文件的忽略
    - 修改 package.json，添加 NODE_ENV 环境变量
    - 更新 app.module.ts，集成 ConfigModule 并配置环境变量加载
    - 重构 main.ts，

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ListModule } from './modules/list/list.module';
 
 @Module({
-  imports: [ListModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+      // 指定多个 env 文件时，第一个优先级最高
+      envFilePath: ['.env.local', `.env.${process.env.NODE_ENV}`, '.env'],
+    }),
+    ListModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 import { CustomValidationPipe } from '@/common/pipes';
 import { ResponseInterceptor, CustomLogger } from '@/common/interceptors';
 import { getLocalIP } from '@/utils/getLocalIP';
-import { getCorsOption } from '@/config/cors';
+import { getCorsOption } from '@/utils/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  // 设置 api 访问前缀
+  const prefix = config.get('API_PREFIX');
+  const port = config.get('APP_PORT') || 3000;
+  app.setGlobalPrefix(prefix);
 
   app.useGlobalPipes(new CustomValidationPipe());
   app.useGlobalInterceptors(new ResponseInterceptor());
@@ -20,7 +28,6 @@ async function bootstrap() {
     }),
   );
 
-  const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
 
   // await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
