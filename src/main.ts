7d27c30ba336a61d52aa01735d8ce0fb4b13ff01
Sun <wanglt-123@qq.com>
2025-09-08 14:36:38 +0800

    build(deps): 更新项目依赖版本
    - 将 @types/node 从 22.10.7 升级到 24.3.1
    - 将 eslint 从 9.18.0 升级到 9.35.0
    - 将 jest 从 30.0.0 升级到 30.1.3

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
-/*
- * @Author: Sun wanglt-123@qq.com
- * @Date: 2025-09-02 09:49:04
- * @LastEditors: Sun wanglt-123@qq.com
- * @LastEditTime: 2025-09-08 11:47:31
- * @FilePath: \nest-prisma\src\main.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { NestExpressApplication } from '@nestjs/platform-express';
