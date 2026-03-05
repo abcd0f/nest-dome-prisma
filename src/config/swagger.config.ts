d6dc204202d1ca1cfc228de4dd287e3d50d54b19
Sun <wanglt-123@qq.com>
2026-03-05 15:11:49 +0800

    feat(config): 添加Swagger配置注释说明
    为swagger配置项添加中文注释，说明各配置项的作用：
    - enable: 是否启用swagger
    - path: swagger文档路径
    - serverUrl: swagger文档服务地址

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -5,8 +5,13 @@ import { env, envBoolean } from '@/utils/globalenv.utils';
 export const swaggerRegToken = 'swagger';
 
 export const SwaggerConfig = registerAs(swaggerRegToken, () => ({
+  // 是否启用 swagger
   enable: envBoolean('SWAGGER_ENABLE'),
+
+  // swagger 文档路径
   path: env('SWAGGER_PATH'),
+
+  // swagger 文档服务地址
   serverUrl: env('SWAGGER_SERVER_URL', env('APP_BASE_URL')),
 }));
 
